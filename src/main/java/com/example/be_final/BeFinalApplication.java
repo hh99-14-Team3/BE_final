d52658803dcd7287b41ccd88782842a011688205
been1118 <dsin1118@kakao.com>
2023-05-19 15:36:14 +0900

    Init : 레포지토리 생성

---
+++ src/main/java/com/example/be_final/BeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/be_final/BeFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
