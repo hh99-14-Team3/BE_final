f588a6bb925ab238d7d14932afb669f3bd89797e
mansooonuna <parkdasom95@gmail.com>
2023-05-20 09:42:42 +0900

    Feat : UserDetailsImpl 추가

---
+++ src/main/java/com/jeju/be_final/userDetails/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.jeju.be_final.userDetails;
+
+
+import com.jeju.be_final.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        Member member = memberRepository.findByEmail(email)
+//                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+//        return new UserDetailsImpl(member, member.getEmail());
+    //TODO : member entity 추가하면 주석 해제, 밑에 null 삭제
+        return null;
+    }
+
+
+}
