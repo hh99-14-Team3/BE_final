0b5e5a978231823178f1191c4b4b735d0790fb6f
been1118 <dsin1118@kakao.com>
2023-05-25 04:32:34 +0900

    Feat : ChatRoomEnterDataRequestDto 구현

---
+++ src/main/java/com/mogakko/be_final/domain/chatroom/dto/request/ChatRoomEnterDataRequestDto.java
@@ -0,0 +1,12 @@
+package com.mogakko.be_final.domain.chatroom.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class ChatRoomEnterDataRequestDto {
+
+    private String password;
+
+}
