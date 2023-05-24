6bb18efb652529aa15b9835d24f1bbd4d289989d
mansooonuna <parkdasom95@gmail.com>
2023-05-25 04:21:21 +0900

    Refactor : 에러코드 추가 및 기타 코드 리팩토링

--- src/main/java/com/mogakko/be_final/util/Message.java
+++ src/main/java/com/mogakko/be_final/util/Message.java
@@ -6,20 +6,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class Message {
-    private int statusCode;
     private String message;
     private Object data;
 
-    public Message(int statusCode, String message) {
-        this.statusCode = statusCode;
-        this.message = message;
-    }
 
-    public static Message setSuccess(StatusEnum statusEnum, String message, Object data) {
-        return new Message(statusEnum.getStatus().value(), message, data);
-    }
-
-    public static Message setSuccess(StatusEnum statusEnum, String message) {
-        return new Message(statusEnum.getStatus().value(), message);
+    public static Message setSuccess(String message, Object data) {
+        return new Message(message, data);
     }
 }
