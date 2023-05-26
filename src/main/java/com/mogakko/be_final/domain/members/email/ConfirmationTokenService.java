3ba0226202c37f82493c25b785fd45c89aae66e0
mansooonuna <parkdasom95@gmail.com>
2023-05-26 16:37:33 +0900

    Feat : 이메일 인증은 비밀번호 찾기위해 사용하기로 기획 변경됨
    따라서 이메일로 변경 비밀번호를 입력할 수 있는 링크로 연결해주고 그 링크를 통해 이메일 변경 진행

---
+++ src/main/java/com/mogakko/be_final/domain/members/email/ConfirmationTokenService.java
@@ -0,0 +1,21 @@
+package com.mogakko.be_final.domain.members.email;
+
+import com.mogakko.be_final.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.mogakko.be_final.exception.ErrorCode.USED_TOKEN;
+
+@RequiredArgsConstructor
+@Service
+public class ConfirmationTokenService {
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public ConfirmationToken findByIdAndExpired(String confirmationTokenId){
+        Optional<ConfirmationToken> confirmationToken = confirmationTokenRepository.findByIdAndExpired(confirmationTokenId, false);
+        return confirmationToken.orElseThrow(()-> new CustomException(USED_TOKEN));
+    };
+
+}
