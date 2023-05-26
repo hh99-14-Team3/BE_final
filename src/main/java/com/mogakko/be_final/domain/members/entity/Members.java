09ff9d94d7b1863ad309159b703e3a0d25d8a35c
mansooonuna <parkdasom95@gmail.com>
2023-05-26 16:47:08 +0900

    Feat : 마이페이지 조회 기능 추가 및 이메일 인증 ->pw 변경 로직 추가,  기타 반환값 리팩토링

--- src/main/java/com/mogakko/be_final/domain/members/entity/Members.java
+++ src/main/java/com/mogakko/be_final/domain/members/entity/Members.java
@@ -1,5 +1,6 @@
 package com.mogakko.be_final.domain.members.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mogakko.be_final.util.Timestamped;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,32 +25,29 @@ public class Members extends Timestamped {
     private String nickname;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 
     @Column(nullable = false)
+    @JsonIgnore
     private boolean isAgreed;
-
-    @Column(nullable = false)
-    private boolean emailAuth;
 
     @Column
     @Lob
-    private String profileImage;
+    private String profileImage;        //TODO : 디폴트 프사 생기면 url 넣을 예졍
 
-    @Column
-    private String socialUid;
 
-    public Members( String email, String nickname, String password, boolean isAgreed, boolean emailAuth){
+    public Members( String email, String nickname, String password, boolean isAgreed){
         this.email = email;
         this.nickname = nickname;
         this.password = password;
         this.isAgreed = isAgreed;
-        this.emailAuth = emailAuth;
     }
 
-    public void emailVerifiedSuccess() {
-        this.emailAuth = true;
+    public void changePassword(String password) {
+        this.password = password;
     }
+
 
 //    public Members kakaoIdUpdate(Long kakaoId) {
 //        this.kakaoId = kakaoId;
