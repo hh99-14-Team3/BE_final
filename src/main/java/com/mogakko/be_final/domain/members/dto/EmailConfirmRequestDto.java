7aa5c3bb278af14212a47ffaece30500781dfcf6
mansooonuna <parkdasom95@gmail.com>
2023-05-26 16:42:37 +0900

    Feat : 이메일 인증 방법 기획 변경으로 필요한 부분 추가

---
+++ src/main/java/com/mogakko/be_final/domain/members/dto/EmailConfirmRequestDto.java
@@ -0,0 +1,10 @@
+package com.mogakko.be_final.domain.members.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailConfirmRequestDto {
+    private String email;
+}
