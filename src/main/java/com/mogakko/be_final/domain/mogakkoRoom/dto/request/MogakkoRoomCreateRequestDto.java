d33b1fbe4739365500774518b409b242324e43ac
been1118 <dsin1118@kakao.com>
2023-05-26 17:06:12 +0900

    Refactor, Feat : MogakkoService 전제적인 리펙토링 인기 모각코 조회 기능 구현

--- src/main/java/com/mogakko/be_final/domain/mogakkoRoom/dto/request/MogakkoRoomCreateRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/mogakkoRoom/dto/request/MogakkoRoomCreateRequestDto.java
@@ -1,6 +1,6 @@
-package com.mogakko.be_final.domain.chatroom.dto.request;
+package com.mogakko.be_final.domain.mogakkoRoom.dto.request;
 
-import com.mogakko.be_final.domain.chatroom.entity.LanguageEnum;
+import com.mogakko.be_final.domain.mogakkoRoom.entity.LanguageEnum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,12 +9,12 @@ import javax.validation.constraints.NotNull;
 
 @Getter
 @RequiredArgsConstructor
-public class ChatRoomCreateRequestDto {
+public class MogakkoRoomCreateRequestDto {
 
     @NotBlank(message = "제목을 입력해 주세요!")
     private String title;
 
-    @NotBlank(message = "주특기 언어를 선택해 주세요!")
+    @NotNull(message = "주특기 언어를 선택해 주세요!")
     private LanguageEnum language;
 
     @NotNull(message = "최대 인원을 설정해주세요!")
@@ -22,6 +22,9 @@ public class ChatRoomCreateRequestDto {
 
     @NotNull(message = "방의 상태를 설정해 주세요!")
     private Boolean isOpened;
+
+    @NotNull(message = "동네를 설정해 주세요!")
+    private String dongNe;
 
     private String password;
 
