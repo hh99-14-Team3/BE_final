f307583e6d622f9a9eea62e91b700b076bf65989
been1118 <dsin1118@kakao.com>
2023-05-31 23:26:45 +0900

    Fix : jwtExceptionHandler Method 예외처리 http 상태코드 정정

--- src/main/java/com/mogakko/be_final/security/jwt/JwtAuthFilter.java
+++ src/main/java/com/mogakko/be_final/security/jwt/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import com.mogakko.be_final.domain.members.repository.MembersRepository;
 import com.mogakko.be_final.util.Message;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -70,7 +71,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
 
     public void jwtExceptionHandler(HttpServletResponse response, String msg) {
-
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
         try {
             String json = new ObjectMapper().writeValueAsString(new Message(msg, null));
