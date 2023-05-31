c3fe1d1a2a76eefec372d8b409f694dab8af2faf
Shuan75 <limbboy@nate.com>
2023-06-01 01:26:00 +0900

    Feat : 친구 요청 보낼시 수락 , 거절 추가

--- src/main/java/com/mogakko/be_final/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/repository/FriendshipRepository.java
@@ -1,6 +1,7 @@
 package com.mogakko.be_final.domain.friendship.repository;
 
 import com.mogakko.be_final.domain.friendship.entity.Friendship;
+import com.mogakko.be_final.domain.friendship.entity.FriendshipStatus;
 import com.mogakko.be_final.domain.members.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     Optional<Friendship> findAllBySenderOrReceiver(Members sender, Members receiver);
+
+    Friendship findBySenderAndReceiverAndStatus(Members sender, Members receiver, FriendshipStatus status);
 }
