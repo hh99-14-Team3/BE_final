15787f28b0f61caeb6f8c63f25337fe78fc14163
bheejun <bhjun95@gmail.com>
2023-06-04 01:29:12 +0900

    Feat: 알림 조회 기능 추가
    토큰이 들어오면 해당 사용자가 수신한 알림을 목록으로 나타냅니다.

--- src/main/java/com/mogakko/be_final/domain/sse/service/NotificationSearchService.java
+++ src/main/java/com/mogakko/be_final/domain/sse/service/NotificationSearchService.java
@@ -1,18 +1,30 @@
 package com.mogakko.be_final.domain.sse.service;
 
-import com.amazonaws.Response;
+import com.mogakko.be_final.domain.members.entity.Members;
+import com.mogakko.be_final.domain.sse.entity.Notification;
+import com.mogakko.be_final.domain.sse.repository.NotificationRepository;
 import com.mogakko.be_final.userDetails.UserDetailsImpl;
 import com.mogakko.be_final.util.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationSearchService {
+    private final NotificationRepository notificationRepository;
     public ResponseEntity<Message> getMyNotification(UserDetailsImpl userDetails){
+        Members receiver = userDetails.getMember();
+        List<Notification> notificationList = notificationRepository.findAllByReceiver(receiver);
 
-        return null;
+        if(!notificationList.isEmpty()){
+            return new ResponseEntity<>(new Message("알림 조회 완료", notificationList), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(new Message("알림이 없습니다.", null), HttpStatus.OK);
+        }
 
 
     }
