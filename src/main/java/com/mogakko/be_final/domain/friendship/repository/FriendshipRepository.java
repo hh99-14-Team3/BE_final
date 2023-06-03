32cedfe8556792c118043fcb4b1e5c3faa30d4c7
bheejun <bhjun95@gmail.com>
2023-06-04 01:41:18 +0900

    Feat : 현재 수신 된 친구 목록 조회 기능 추가
    사용자가 receiver 이고 요청 상태가 PENDING 인 목록을 조회 할 수 있도록 했습니다.

--- src/main/java/com/mogakko/be_final/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/repository/FriendshipRepository.java
@@ -13,6 +13,10 @@ public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     Optional<Friendship> findAllBySenderAndReceiver(Members sender, Members receiver);
 
     Optional<Friendship> findAllBySenderOrReceiver(Members sender, Members receiver);
+
     Friendship findBySenderAndReceiverAndStatus(Members sender, Members receiver, FriendshipStatus status);
+
     List<Friendship> findAllBySenderOrReceiverAndStatus(Members sender, Members receiver, FriendshipStatus status);
+
+    List<Friendship> findAllByReceiverAndStatus(Members receiver, FriendshipStatus status);
 }
--- src/main/java/com/mogakko/be_final/domain/friendship/service/FriendshipSearchService.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/service/FriendshipSearchService.java
@@ -5,8 +5,7 @@ import com.mogakko.be_final.domain.friendship.entity.FriendshipStatus;
 import com.mogakko.be_final.domain.friendship.repository.FriendshipRepository;
 import com.mogakko.be_final.domain.members.entity.Members;
 import com.mogakko.be_final.domain.members.repository.MembersRepository;
-import com.mogakko.be_final.exception.CustomException;
-import com.mogakko.be_final.exception.ErrorCode;
+import com.mogakko.be_final.userDetails.UserDetailsImpl;
 import com.mogakko.be_final.util.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,15 +21,15 @@ public class FriendshipSearchService {
     private final FriendshipRepository friendshipRepository;
     private final MembersRepository membersRepository;
 
-    public ResponseEntity<Message> getMyFriend(Long memberId){
-        Members member = membersRepository.findById(memberId).orElseThrow(
-                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
-        );
+    public ResponseEntity<Message> getMyFriend(UserDetailsImpl userDetails){
+        Members member = userDetails.getMember();
+        Long memberId = member.getId();
+
         List<Long> friendsID = new ArrayList<>();
 
         List<Friendship> findList = friendshipRepository.findAllBySenderOrReceiverAndStatus(member, member, FriendshipStatus.ACCEPT);
         if (findList==null){
-            return new ResponseEntity<>(new Message("친구가 없습니다.", null), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(new Message("조회된 친구가 없습니다.", null), HttpStatus.NOT_FOUND);
         }
         for (Friendship friendship : findList) {
 
@@ -52,6 +51,26 @@ public class FriendshipSearchService {
         }
 
         return new ResponseEntity<>(new Message("친구 목록 조회 성공", friendsList), HttpStatus.OK);
+
+    }
+
+
+    public ResponseEntity<Message> getMyFriendRequest(UserDetailsImpl userDetails){
+        Members receiver = userDetails.getMember();
+
+        List<Friendship> friendRequests = friendshipRepository.findAllByReceiverAndStatus(receiver, FriendshipStatus.PENDING);
+        List<Members> friendRequestSenderList = new ArrayList<>();
+
+        if(friendRequests.isEmpty()){
+            return new ResponseEntity<>(new Message("수신된 친구 요청이 없습니다", null),HttpStatus.OK);
+        }else{
+            for (Friendship friendRequest : friendRequests) {
+                Members requestSender = friendRequest.getSender();
+                friendRequestSenderList.add(requestSender);
+            }
+            return new ResponseEntity<>(new Message("친구 요청 목록 조회 성공", friendRequestSenderList),HttpStatus.OK);
+        }
+
 
     }
 
