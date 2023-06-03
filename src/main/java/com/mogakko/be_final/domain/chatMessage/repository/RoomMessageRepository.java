8131bd55b882b01928c52860a8d75d8c56b20525
mansooonuna <parkdasom95@gmail.com>
2023-06-04 03:47:12 +0900

    Refactor : 채팅 메세지는 레디스에 저장하여 관리함 -> 기존 repository 삭제

--- src/main/java/com/mogakko/be_final/domain/chatMessage/repository/RoomMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mogakko.be_final.domain.chatMessage.repository;
-
-import com.mogakko.be_final.domain.chatMessage.entity.RoomMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomMessageRepository extends JpaRepository<RoomMessage, Long> {
-}
