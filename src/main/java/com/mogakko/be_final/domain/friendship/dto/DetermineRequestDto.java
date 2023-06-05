bbebf6e48af7a7fb51fdcf5e142b372e6a748946
bheejun <bhjun95@gmail.com>
2023-06-05 22:48:51 +0900

    Refactor : 친구 기능 코드 전체적으로 리팩토링 진행
    Dto 에서 불필요하게 받는 변수 제거 및 email 에서 nickname 으로 변수가 바뀜에 따라 코드 리팩토링 , 토큰 활용한 사용자 인증 추가에 따른 코드 리팩토링, Errcode 추가

--- src/main/java/com/mogakko/be_final/domain/friendship/dto/DetermineRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/dto/DetermineRequestDto.java
@@ -6,16 +6,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class DetermineRequestDto {
-    private Long notificationId;
-    private String senderNickname;
-    private String receiverNickname;
+    private String requestSenderNickname;//알림 기준으로 sender
     private boolean determineRequest; // true 친구수락, false 거절
-
-    public DetermineRequestDto(Long notificationId, String senderNickname, String receiverNickname, boolean determineRequest) {
-        this.notificationId = notificationId;
-        this.senderNickname = senderNickname;
-        this.receiverNickname = receiverNickname;
-        this.determineRequest = determineRequest;
-    }
 
 }
