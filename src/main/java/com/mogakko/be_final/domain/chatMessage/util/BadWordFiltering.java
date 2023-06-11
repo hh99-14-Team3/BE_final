85d254936c60d3b846f27fb26d79e95b403d1eb9
been1118 <dsin1118@kakao.com>
2023-06-12 04:36:26 +0900

    Chore : BadWordFiltering 설명 주석 추가

--- src/main/java/com/mogakko/be_final/domain/chatMessage/util/BadWordFiltering.java
+++ src/main/java/com/mogakko/be_final/domain/chatMessage/util/BadWordFiltering.java
@@ -15,20 +15,27 @@ public class BadWordFiltering implements BadWords {
 
     public ChatMessage checkBadWord(ChatMessage chatMessage) {
         String text = chatMessage.getMessage();
+
+        // 문자열을 빌더 패턴을 사용하여 생성
         StringBuilder singBuilder = new StringBuilder("[");
         for (String sing : sings) singBuilder.append(Pattern.quote(sing));
         singBuilder.append("]*");
         String patternText = singBuilder.toString();
 
+        // set에 있는 각 단어에 대해 필터링 작업을 수행
         for (String word : set) {
+            // 단어의 길이가 1이면 단어를 대체값(substituteValue)으로 치환
             if (word.length() == 1) text = text.replace(word, substituteValue);
+
+            // 단어를 문자 단위로 분할헤서 배열에 저장
             String[] chars = word.split("");
+
+            // 패턴을 생성하여 문자 단위로 일치하는 부분을 대체값으로 치환
             text = Pattern.compile(String.join(patternText, chars))
                     .matcher(text)
                     .replaceAll(v -> substituteValue.repeat(v.group().length()));
         }
         chatMessage.setMessage(text);
-
         return chatMessage;
     }
 }
