bcc40c8964303bd427caec8797b9e24b23164b21
been1118 <dsin1118@kakao.com>
2023-06-13 17:06:27 +0900

    Refactor : 모각코On:s 조회 위한 쿼리 작성, 일주일마다 초기화 스케쥴러 쓰기 위한 쿼리 작성

--- src/main/java/com/mogakko/be_final/domain/members/repository/MemberWeekStatisticsRepository.java
+++ src/main/java/com/mogakko/be_final/domain/members/repository/MemberWeekStatisticsRepository.java
@@ -3,6 +3,11 @@ package com.mogakko.be_final.domain.members.repository;
 import com.mogakko.be_final.domain.members.entity.MemberWeekStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberWeekStatisticsRepository extends JpaRepository<MemberWeekStatistics, String> {
 
+    List<MemberWeekStatistics> findTop8ByOrderByWeekTotalTimeDesc();
+
+    MemberWeekStatistics findByEmail(String email);
 }
