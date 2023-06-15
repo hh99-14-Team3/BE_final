183b25fa7a3f292d91df440e7f5c575184882e01
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-06-15 21:24:11 +0900

    Docs : Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,8 +13,8 @@ echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
   sleep 10
 fi
 
