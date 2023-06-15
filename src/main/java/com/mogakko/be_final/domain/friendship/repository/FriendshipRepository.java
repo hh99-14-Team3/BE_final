c6b4cf9176bcb3a6f2771ed825be3dd9e90c5092
mansooonuna <parkdasom95@gmail.com>
2023-06-15 21:51:38 +0900

    Refactor : Friend 관련 코드라인정리/ delete unused

--- src/main/java/com/mogakko/be_final/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/repository/FriendshipRepository.java
@@ -3,11 +3,7 @@ package com.mogakko.be_final.domain.friendship.repository;
 import com.mogakko.be_final.domain.friendship.entity.Friendship;
 import com.mogakko.be_final.domain.friendship.entity.FriendshipStatus;
 import com.mogakko.be_final.domain.members.entity.Members;
-import jnr.a64asm.OP;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 
 import java.util.List;
 import java.util.Optional;
@@ -15,13 +11,9 @@ import java.util.Optional;
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     Optional<Friendship> findBySenderAndReceiver(Members sender, Members receiver);
 
-    Optional<Friendship> findBySenderOrReceiver(Members sender, Members receiver);
-
     Optional<Friendship> findBySenderAndReceiverAndStatus(Members sender, Members receiver, FriendshipStatus status);
 
-    List<Friendship> findAllBySenderOrReceiverAndStatus(Members sender, Members receiver, FriendshipStatus status);
-
-    List<Friendship> findAllBySenderAndStatus(Members sender,FriendshipStatus status);
+    List<Friendship> findAllBySenderAndStatus(Members sender, FriendshipStatus status);
 
     List<Friendship> findAllByReceiverAndStatus(Members receiver, FriendshipStatus status);
 }
