72165b2ee36c72ad2ac6d0a9656eae68de1cd9c0
mansooonuna <parkdasom95@gmail.com>
2023-06-15 16:51:56 +0900

    Refactor : NotificationSendService url 하드코딩 제거

--- src/main/java/com/mogakko/be_final/domain/sse/service/NotificationSendService.java
+++ src/main/java/com/mogakko/be_final/domain/sse/service/NotificationSendService.java
@@ -3,6 +3,7 @@ package com.mogakko.be_final.domain.sse.service;
 import com.mogakko.be_final.domain.members.entity.Members;
 import com.mogakko.be_final.domain.sse.entity.NotificationType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,21 +11,24 @@ import org.springframework.stereotype.Service;
 public class NotificationSendService {
     private final NotificationService notificationService;
 
-    public void sendFriendRequestNotification(Members sender, Members receiver){
-        String content = sender.getNickname() + "님이 친구요청을 보냈습니다.";
-        String url = "https://api.abc123poi.com/friendship/requests/pending";
-       
-      notificationService.send(sender, receiver, NotificationType.FRIEND_REQUEST, content, url);
-    }
+    @Value("${SERVER_URL}")
+    private String SERVER_URL;
 
-    public void sendAcceptNotification(Members sender, Members receiver){
-        String content = sender.getNickname() + "님이 친구요청을 수락하셨습니다.";
-        String url = "https://api.abc123poi.com/friendship/requests/accepted";
+    public void sendFriendRequestNotification(Members sender, Members receiver) {
+        String content = sender.getNickname() + "님이 친구요청을 보냈습니다.";
+        String url = SERVER_URL + "/friendship/requests/pending";
 
         notificationService.send(sender, receiver, NotificationType.FRIEND_REQUEST, content, url);
     }
 
-    public void sendRefuseNotification(Members sender, Members receiver){
+    public void sendAcceptNotification(Members sender, Members receiver) {
+        String content = sender.getNickname() + "님이 친구요청을 수락하셨습니다.";
+        String url = SERVER_URL + "/friendship/requests/accepted";
+
+        notificationService.send(sender, receiver, NotificationType.FRIEND_REQUEST, content, url);
+    }
+
+    public void sendRefuseNotification(Members sender, Members receiver) {
         String content = sender.getNickname() + "님이 친구요청을 거절하셨습니다.";
         String url = "/friend/request/determine";
 
@@ -33,10 +37,8 @@ public class NotificationSendService {
 
     public void sendMessageReceivedNotification(Members sender, Members receiver) {
         String content = sender.getNickname() + "님으로부터 쪽지가 도착했습니다.";
-        String url = "https://api.abc123poi.com/directMessage/received";
+        String url = SERVER_URL + "/directMessage/received";
 
         notificationService.send(sender, receiver, NotificationType.MESSAGE, content, url);
     }
-  
-
 }
