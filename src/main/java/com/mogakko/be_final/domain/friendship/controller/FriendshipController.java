9eb936b03c330205e43624dfcc4ffce2c36ecf47
been1118 <dsin1118@kakao.com>
2023-06-15 20:28:02 +0900

    Fix : FriendshipController commit 누락사항 수정

--- src/main/java/com/mogakko/be_final/domain/friendship/controller/FriendshipController.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/controller/FriendshipController.java
@@ -1,9 +1,7 @@
 package com.mogakko.be_final.domain.friendship.controller;
 
-import com.mogakko.be_final.domain.friendship.dto.DeleteFriendRequestDto;
-import com.mogakko.be_final.domain.friendship.dto.DetermineRequestDto;
-import com.mogakko.be_final.domain.friendship.dto.FriendRequestByCodeDto;
-import com.mogakko.be_final.domain.friendship.dto.FriendRequestDto;
+import com.mogakko.be_final.domain.friendship.dto.*;
+import com.mogakko.be_final.domain.friendship.service.FriendshipSearchService;
 import com.mogakko.be_final.domain.friendship.service.FriendshipService;
 import com.mogakko.be_final.userDetails.UserDetailsImpl;
 import com.mogakko.be_final.util.Message;
@@ -12,10 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "친구기능 관련 API", description = "친구기능 관련 API 입니다.")
 @RestController
@@ -23,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FriendshipController {
     private final FriendshipService friendshipService;
+    private final FriendshipSearchService friendshipSearchService;
 
     @PostMapping
     @Operation(summary = "친구 요청 API", description = "친구 요청을 보내는 메서드입니다. 수신자의 nickname을 보내주시면 요청이 완료됩니다.")
@@ -51,4 +47,17 @@ public class FriendshipController {
                                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return friendshipService.deleteFriend(deleteFriendRequestDto, userDetails.getMember());
     }
+
+    @GetMapping("/accepted")
+    @Operation(summary = "친구목록 조회 API", description = "사용자의 친구 목록을 조회하는 메서드입니다.")
+    public ResponseEntity<Message> getMyFriend(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return friendshipSearchService.getMyFriend(userDetails.getMember());
+    }
+
+    @GetMapping("/pending")
+    @Operation(summary = "받은 친구요청 조회 API", description = "사용자의 친구 요청 목록을 조회하는 메서드입니다.")
+    public ResponseEntity<Message> getMyFriendRequest(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return friendshipSearchService.getMyFriendRequest(userDetails.getMember());
+    }
+
 }
