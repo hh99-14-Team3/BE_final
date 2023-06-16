c16a5b6dd45b67f36403b3610b19892d5bb2585c
been1118 <dsin1118@kakao.com>
2023-06-17 03:58:03 +0900

    Feat : 닉네임 욕설 필터링 -> UUID로 대체

--- src/main/java/com/mogakko/be_final/util/BadWordFiltering.java
+++ src/main/java/com/mogakko/be_final/util/BadWordFiltering.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 @Component
@@ -31,6 +32,34 @@ public class BadWordFiltering implements BadWords {
             inputString = Pattern.compile(String.join(patternText, chars))
                     .matcher(inputString)
                     .replaceAll(v -> substituteValue.repeat(v.group().length()));
+        }
+        return inputString;
+    }
+
+    public String checkBadWordUUID(String inputString) {
+        // 문자열을 빌더 패턴을 사용하여 생성
+        StringBuilder stringBuilder = new StringBuilder("[");
+        for (String str : set) {
+            stringBuilder.append(Pattern.quote(str));
+        }
+        stringBuilder.append("]*");
+        String patternText = stringBuilder.toString();
+
+        // set에 있는 각 단어에 대해 필터링 작업을 수행
+        for (String word : set) {
+            // 단어의 길이가 1이면 단어를 대체값(substituteValue)으로 치환
+            if (word.length() == 1) {
+                inputString = inputString.replace(word, substituteValue);
+            } else {
+                // 단어를 문자 단위로 분할해서 배열에 저장
+                String[] chars = word.split("");
+
+                // 패턴을 생성하여 문자 단위로 일치하는 부분을 UUID로 대체
+                String uuid = UUID.randomUUID().toString().substring(0, 10);
+                inputString = Pattern.compile(String.join(patternText, chars))
+                        .matcher(inputString)
+                        .replaceAll(uuid);
+            }
         }
         return inputString;
     }
