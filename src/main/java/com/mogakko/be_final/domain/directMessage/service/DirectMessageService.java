ca866965b6be5b4ad1100868ed51c0ebb344427f
been1118 <dsin1118@kakao.com>
2023-06-17 13:49:50 +0900

    Refactor : DirectMessageService 하나로 병합

--- src/main/java/com/mogakko/be_final/domain/directMessage/service/DirectMessageService.java
+++ src/main/java/com/mogakko/be_final/domain/directMessage/service/DirectMessageService.java
@@ -1,11 +1,15 @@
 package com.mogakko.be_final.domain.directMessage.service;
 
-import com.mogakko.be_final.domain.directMessage.dto.DirectMessageSearchResponseDto;
+import com.mogakko.be_final.domain.directMessage.dto.request.DirectMessageSendRequestDto;
+import com.mogakko.be_final.domain.directMessage.dto.response.DirectMessageSearchResponseDto;
 import com.mogakko.be_final.domain.directMessage.entity.DirectMessage;
 import com.mogakko.be_final.domain.directMessage.repository.DirectMessageRepository;
 import com.mogakko.be_final.domain.members.entity.Members;
+import com.mogakko.be_final.domain.members.repository.MembersRepository;
+import com.mogakko.be_final.domain.sse.service.NotificationSendService;
 import com.mogakko.be_final.exception.CustomException;
 import com.mogakko.be_final.exception.ErrorCode;
+import com.mogakko.be_final.util.BadWordFiltering;
 import com.mogakko.be_final.util.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +20,49 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mogakko.be_final.exception.ErrorCode.USER_NOT_FOUND;
+
 @Service
 @RequiredArgsConstructor
-public class DirectMessageSearchService {
+public class DirectMessageService {
+    private final BadWordFiltering badWordFiltering;
+    private final NotificationSendService notificationSendService;
+    private final MembersRepository membersRepository;
     private final DirectMessageRepository directMessageRepository;
+
+    // 쪽지 전송
+    @Transactional
+    public ResponseEntity<Message> sendDirectMessage(Members member, DirectMessageSendRequestDto directMessageSendRequestDto) {
+        Members messageReceiver = membersRepository.findByNickname(directMessageSendRequestDto.getMessageReceiverNickname()).orElseThrow(
+                () -> new CustomException(USER_NOT_FOUND)
+        );
+        String messageContent = badWordFiltering.checkBadWord(directMessageSendRequestDto.getContent());
+
+        if (messageReceiver.getNickname().equals(member.getNickname())) {
+            return new ResponseEntity<>(new Message("자신에게는 쪽지를 보낼 수 없습니다.", null), HttpStatus.BAD_REQUEST);
+        }
+
+        if (messageContent.isEmpty()) {
+            return new ResponseEntity<>(new Message("내용을 입력해 주세요.", null), HttpStatus.BAD_REQUEST);
+        }
+
+        DirectMessage directMessage = new DirectMessage(member, messageReceiver, messageContent, false);
+        directMessageRepository.save(directMessage);
+        notificationSendService.sendMessageReceivedNotification(member, messageReceiver);
+
+        return new ResponseEntity<>(new Message("쪽지 전송 성공", null), HttpStatus.OK);
+    }
+
+//    @Transactional
+//    public ResponseEntity<Message> deleteDirectMessage(DirectMessageDeleteRequestDto requestDto, Members member) {
+//        List<Long> dmList = requestDto.getDirectMessageList();
+//
+//        for (Long dm : dmList) {
+//            DirectMessage directMessage = findDirectMessageById(dm);
+//            directMessageRepository.delete(directMessage);
+//        }
+//        return new ResponseEntity<>(new Message("쪽지 삭제 성공", null), HttpStatus.OK);
+//    }
 
     @Transactional(readOnly = true)
     public ResponseEntity<Message> searchReceivedMessage(Members member) {
@@ -60,14 +103,22 @@ public class DirectMessageSearchService {
 
     @Transactional(readOnly = true)
     public ResponseEntity<Message> readDirectMessage(Members member, Long messageId) {
-        DirectMessage findMessage = directMessageRepository.findById(messageId).orElseThrow(
-                () -> new CustomException(ErrorCode.MESSAGE_NOT_FOUND)
-        );
+        DirectMessage findMessage = findDirectMessageById(messageId);
         if (member == findMessage.getReceiver()) {
             findMessage.markRead();
             return new ResponseEntity<>(new Message("쪽지 조회 완료", findMessage), HttpStatus.OK);
         } else {
             throw new CustomException(ErrorCode.INTERNAL_SERER_ERROR);
         }
+    }
+
+    /**
+     * Method
+     */
+
+    private DirectMessage findDirectMessageById(Long id) {
+        return directMessageRepository.findById(id).orElseThrow(
+                () -> new CustomException(ErrorCode.MESSAGE_NOT_FOUND)
+        );
     }
 }
