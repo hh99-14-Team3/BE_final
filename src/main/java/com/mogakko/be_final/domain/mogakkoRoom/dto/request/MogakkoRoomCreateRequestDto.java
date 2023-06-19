d5c98197a0528139e04b4b2cc7d0dbc2fdd8d941
mansooonuna <parkdasom95@gmail.com>
2023-06-19 20:18:51 +0900

    Refactor : #156 모각코 룸 만들 때 방이름 글자수 제한 설

--- src/main/java/com/mogakko/be_final/domain/mogakkoRoom/dto/request/MogakkoRoomCreateRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/mogakkoRoom/dto/request/MogakkoRoomCreateRequestDto.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -14,6 +15,7 @@ import javax.validation.constraints.NotNull;
 public class MogakkoRoomCreateRequestDto {
 
     @NotBlank(message = "제목을 입력해 주세요!")
+    @Size(min = 1, max = 8, message = "모각코 이름은 15글자 이내로 생성해주세요")
     private String title;
 
     @NotNull(message = "주특기 언어를 선택해 주세요!")
