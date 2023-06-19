9a2ec4337f61c3c1c75d368dd0e3b898797a5808
mansooonuna <parkdasom95@gmail.com>
2023-06-19 09:50:27 +0900

    Refactor : 유저 피드백 반영 회원가입 유효성검사 완화

--- src/main/java/com/mogakko/be_final/domain/members/dto/request/SignupRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/members/dto/request/SignupRequestDto.java
@@ -20,8 +20,8 @@ public class SignupRequestDto {
     @Size(min = 2, max = 8, message = "닉네임은 2~8글자로 생성해주세요")
     private String nickname;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,16}$",
-            message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z]).{6,16}$",
+            message = "비밀번호는 6~16자 영문, 숫자를 사용하세요.")
     private String password;
 
 }
