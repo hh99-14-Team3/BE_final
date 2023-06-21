ff50c82628aba3629745864ac69d30dd637b005f
bheejun <bhjun95@gmail.com>
2023-06-21 23:53:21 +0900

    Feat : Redis를 활용한 친구요청 거절 상태 관리 기능 추가
    Redis 의 TTL 기능을 활용해 일정 시간이 지나면 다시 친구 요청을 할 수 있도록 기능을 구현 해 보았습니다. 그리고 거절 상태일 시, 만료 시간이 얼마나 남았는지 반환하도록 했습니다. 우선은 만료 시간을 하루로 설정해 두었는데, 다 같이 상의해서 어느 정도 시간이 적당할지 정하면 좋을 것 같습니다.

--- src/main/java/com/mogakko/be_final/redis/util/RedisUtil.java
+++ src/main/java/com/mogakko/be_final/redis/util/RedisUtil.java
@@ -1,5 +1,6 @@
 package com.mogakko.be_final.redis.util;
 
+import com.mogakko.be_final.domain.friendship.entity.RejectedFriendship;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -13,6 +14,7 @@ public class RedisUtil {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final RedisTemplate<String, String> redisBlackListTemplate;
+    private final RedisTemplate<String, Object> redisFriendshipTemplate;
 
     public void set(String key, String val, long time) {
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(val.getClass()));
@@ -50,5 +52,17 @@ public class RedisUtil {
 
     public boolean hasKeyBlackList(String key) {
         return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(key));
+    }
+
+    public void setRejectedFriendshipWithExpireTime(String key, Object value, long timeout, TimeUnit unit) {
+       redisFriendshipTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(RejectedFriendship.class));
+       redisFriendshipTemplate.opsForValue().set(key, value, timeout, unit);    }
+
+    public boolean hasKeyFriendship(String key) {
+        return Boolean.TRUE.equals(redisFriendshipTemplate.hasKey(key));
+    }
+
+    public Long getExpire(String key, TimeUnit unit) {
+        return redisTemplate.getExpire(key, unit);
     }
 }
