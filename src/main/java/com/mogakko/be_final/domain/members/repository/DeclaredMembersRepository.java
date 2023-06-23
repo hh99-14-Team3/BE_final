5604a93e2783e26b0cfe56b23734e89ae67af65f
been1118 <dsin1118@kakao.com>
2023-06-23 21:42:40 +0900

    Refactor : DeclaredMembersRepository 신고된 멤버 조회 메서드 추가

--- src/main/java/com/mogakko/be_final/domain/members/repository/DeclaredMembersRepository.java
+++ src/main/java/com/mogakko/be_final/domain/members/repository/DeclaredMembersRepository.java
@@ -3,5 +3,10 @@ package com.mogakko.be_final.domain.members.repository;
 import com.mogakko.be_final.domain.members.entity.DeclaredMembers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeclaredMembersRepository extends JpaRepository<DeclaredMembers, Long> {
+
+    Optional<DeclaredMembers> findByDeclaredMemberId(Long id);
+
 }
