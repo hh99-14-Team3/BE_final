ba1c50dfe1b73dd18650a827b896a7a74cf0ba9f
bheejun <bhjun95@gmail.com>
2023-06-24 11:53:09 +0900

    Feat : 기존 SseEmitter 활용하는 방식에서 Flux를 활용한 방식으로 SSE 변경
    가장 먼저 Controller 와 Service layer 를 수정 했는데, 기존 SseEmitter 를 활용하던 방식과 가장 크게 바뀐점은 WebClient 기본 단위인 Flux라는 타입을 활용해 SSE를 구현 하게 된 것.
    Flux는 구성하는 모든 요소들이 비차단 / 비동기 방식으로 구현되어야 제 성능을 발휘 하기 때문에 이 조건을 만족하기 위해 Service layer 에서 subscribe에 관여하는 모든 데이터타입이 Flux로 수정됨.

--- src/main/java/com/mogakko/be_final/domain/sse/controller/NotificationController.java
+++ src/main/java/com/mogakko/be_final/domain/sse/controller/NotificationController.java
@@ -1,6 +1,5 @@
 package com.mogakko.be_final.domain.sse.controller;
 
-import com.mogakko.be_final.domain.sse.repository.EmitterRepositoryImpl;
 import com.mogakko.be_final.domain.sse.service.NotificationSearchService;
 import com.mogakko.be_final.domain.sse.service.NotificationService;
 import com.mogakko.be_final.userDetails.UserDetailsImpl;
@@ -10,10 +9,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -26,17 +29,25 @@ public class NotificationController {
     private final NotificationSearchService notificationSearchService;
 
     @Operation(summary = "SSE 구독 API", description = "SSE 구독하는 메서드입니다. 해당 주소로 get 요청시 연결됩니다.")
-    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public SseEmitter subscribe(@AuthenticationPrincipal UserDetailsImpl membersDetails,
-                                @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
-        log.info("===== SSE 구독 확인 : [{}]님 구독", membersDetails.getMember().getNickname());
-        return notificationService.subscribe(membersDetails.getMember().getId(), lastEventId);
+    public Flux<ServerSentEvent<Object>> subscribe(@AuthenticationPrincipal UserDetailsImpl membersDetails) {
+        return notificationService.subscribeWithSuccessMessage(membersDetails.getMember());
     }
 
     @Operation(summary = "SSE 알림 조회 API", description = "SSE 생성된 알림을 조회 하는 메서드입니다.")
     @GetMapping("/notification")
     public ResponseEntity<Message> getAllNotification(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return notificationSearchService.getMyNotification(userDetails);
+    }
+
+    @PostMapping("/notification/read/{notificationId}")
+    public ResponseEntity<Message> readNotification(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable UUID notificationId){
+        return notificationSearchService.readNotification(userDetails.getMember().getId(), notificationId);
+    }
+
+    @PostMapping("/notification/read/all")
+    public ResponseEntity<Message> readAllNotification(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return notificationSearchService.readAllNotification(userDetails.getMember().getId());
     }
 }
--- src/main/java/com/mogakko/be_final/domain/sse/service/NotificationService.java
+++ src/main/java/com/mogakko/be_final/domain/sse/service/NotificationService.java
@@ -1,158 +1,100 @@
 package com.mogakko.be_final.domain.sse.service;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import com.mogakko.be_final.domain.members.entity.Members;
-import com.mogakko.be_final.domain.members.repository.MembersRepository;
 import com.mogakko.be_final.domain.sse.dto.response.NotificationResponseDto;
 import com.mogakko.be_final.domain.sse.entity.Notification;
 import com.mogakko.be_final.domain.sse.entity.NotificationType;
-import com.mogakko.be_final.domain.sse.repository.EmitterRepository;
-import com.mogakko.be_final.domain.sse.repository.NotificationRepository;
-import com.mogakko.be_final.exception.CustomException;
-import com.mogakko.be_final.exception.ErrorCode;
+import com.mogakko.be_final.domain.sse.repository.NotificationReactiveRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.io.IOException;
-import java.time.Clock;
 import java.time.Instant;
-import java.time.ZoneId;
-import java.util.List;
-import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
-    //= new EmitterRepositoryImpl()
-    private final EmitterRepository emitterRepository;
-    private final NotificationRepository notificationRepository;
-    private final MembersRepository membersRepository;
-    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
+    private final NotificationProcessor notificationProcessor;
+    private final NotificationReactiveRepository notificationReactiveRepository;
 
 
-    public SseEmitter subscribe(Long memberId, String lastEventId) {
-        String emitterId = memberId + "_" + System.currentTimeMillis();
-        System.out.println(emitterId);
-        SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
+    public  Flux<ServerSentEvent<Object>> subscribeWithSuccessMessage(Members member){
+        log.info("===== SSE 구독 확인 : [{}]님 구독", member.getNickname());
 
-        emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
-        emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
+        String successMessage = "연결에 성공했습니다. 사용자 닉네임: " + member.getNickname();
 
-        sendToClient(emitter, emitterId, "EventStream Created. [memberId=" + memberId + "]");
+        ServerSentEvent<Object> sendSuccessMessage = ServerSentEvent.builder()
+                .event("notification-event")
+                .data(successMessage)
+                .build();
 
-        Members eventReceiver = membersRepository.findById(memberId).orElseThrow(
-                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+        return Flux.concat(
+                Flux.just(sendSuccessMessage),
+                subscribe(member.getId())
         );
-        List<Notification> missedNotifications = findUnreadNotificationList(eventReceiver.getId());
-
-        for (Notification missedNotification : missedNotifications) {
-            String senderNickname = missedNotification.getSenderNickname();
-            Members sender = membersRepository.findByNickname(senderNickname).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-            String senderProfileUrl = sender.getProfileImage();
-            sendToClient(emitter, emitterId, new NotificationResponseDto(missedNotification, senderProfileUrl), missedNotification);
-        }
-
-
-//        if (!lastEventId.isEmpty()) {
-//            Map<String, Object> events = emitterRepository.findAllEventCacheStartWithByMemberId(String.valueOf(memberId));
-//            events.entrySet().stream()
-//                    .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-//                    .forEach(entry ->{
-//                        NotificationResponseDto responseDto = new NotificationResponseDto((Notification)entry.getValue(), entry.getKey());
-//                        sendToClient(emitter, entry.getKey(), responseDto);
-//                            });
-//
-//        }
-
-        return emitter;
     }
 
+    public Flux<ServerSentEvent<Object>> subscribe(Long memberId) {
+        Flux<Notification> missedNotifications = findUnreadNotificationList(memberId);
 
-    public void send(Members sender, Members receiver, NotificationType notificationType, String content, String url) {
+        Flux<ServerSentEvent<Object>> missedNotificationFlux = missedNotifications
+                .map(notification -> new NotificationResponseDto(notification))
+                .map(notificationResponseDto -> ServerSentEvent.builder()
+                        .event("notification-event")
+                        .data(notificationResponseDto)
+                        .build());
+
+        Flux<ServerSentEvent<Object>> newNotificationFlux = this.notificationProcessor.stream()
+                .filter(notification -> notification.getReceiverId().equals(memberId))
+                .map(notificationResponseDto -> ServerSentEvent.builder()
+                        .event("notification-event")
+                        .data(notificationResponseDto)
+                        .build());
+
+        return Flux.concat(missedNotificationFlux, newNotificationFlux)
+                .onBackpressureBuffer()
+                .doOnError( error -> log.error("Id {} 에 대한 알림을 처리하는 동안 오류가 발생했습니다", memberId))
+                .doOnTerminate(() -> log.info("Id {} 와 연결이 끊겼습니다.", memberId));
+    }
+
+    public Mono<Void> send(Members sender, Members receiver, NotificationType notificationType, String content, String url) {
         Notification notification = createNotification(sender, receiver, notificationType, content, url);
-        notificationRepository.save(notification);
-        String memberId = String.valueOf(receiver.getId());
-
-
-        emitterRepository.saveEventCache(memberId, notification);
-
-        Map<String, SseEmitter> sseEmitters = emitterRepository.findAllEmitterStartWithByMemberId(memberId);
-        sseEmitters.forEach(
-                (key, emitter) -> {
-                    sendToClient(emitter, key, new NotificationResponseDto(notification, sender.getProfileImage()), notification);
-                }
-        );
+        return notificationReactiveRepository.save(notification)
+                .doOnSuccess(savedNotification -> {
+                    NotificationResponseDto notificationResponseDto = new NotificationResponseDto(savedNotification);
+                    this.notificationProcessor.publish(notificationResponseDto);
+                })
+                .then()
+                .doOnError( error -> log.error("Id {} 에 대한 알림을 처리하는 동안 오류가 발생했습니다", receiver.getId()));
     }
 
     private Notification createNotification(Members sender, Members receiver, NotificationType notificationType, String content, String url) {
         return Notification.builder()
                 .receiverId(receiver.getId())
                 .readStatus(false)
+                .notificationId(Uuids.timeBased())
                 .createdAt(Instant.now())
                 .senderNickname(sender.getNickname())
                 .receiverNickname(receiver.getNickname())
                 .content(content)
                 .url(url)
                 .type(notificationType)
+                .senderProfileUrl(sender.getProfileImage())
                 .build();
     }
 
 
-    public void sendToClient(SseEmitter emitter, String emitterId, Object data, Notification notification) {
-        try {
-            emitter.send(SseEmitter.event()
-                    .id(emitterId)
-                    .data(data));
-            if (data instanceof NotificationResponseDto) {
-                markAsRead(notification);
-            }
-        } catch (IOException exception) {
-            emitterRepository.deleteById(emitterId);
-            throw new CustomException(ErrorCode.NOTIFICATION_SENDING_FAILED);
-        }
-    }
 
-    public void sendToClient(SseEmitter emitter, String emitterId, Object data) {
-        try {
-            emitter.send(SseEmitter.event()
-                    .id(emitterId)
-                    .data(data));
-        } catch (IOException exception) {
-            emitterRepository.deleteById(emitterId);
-            throw new CustomException(ErrorCode.NOTIFICATION_SENDING_FAILED);
-        }
-    }
-
-
-    public void markAsRead(Notification notification) {
-        notificationRepository.delete(notification);
-        notification.changeReadStatus();
-        notificationRepository.save(notification);
-    }
-
-
-    @Scheduled(fixedDelay = 30000)  // every 30 seconds
-    public void sendHeartbeat() {
-        Map<String, SseEmitter> sseEmitters = emitterRepository.findAllEmitter();
-        sseEmitters.forEach(
-                (key, emitter) -> {
-                    try {
-                        emitter.send(SseEmitter.event()
-                                .id(key)
-                                .name("heartbeat")
-                                .data(""));
-                    } catch (IOException exception) {
-                        emitterRepository.deleteById(key);
-                    }
-                }
-        );
-    }
-
-    public List<Notification> findUnreadNotificationList(Long memberId) {
-        return notificationRepository.findAllByReceiverIdAndReadStatusAndCreatedAtLessThan(
-                memberId, false, Instant.now(Clock.system(ZoneId.of("Asia/Seoul"))));
+    public Flux<Notification> findUnreadNotificationList(Long memberId){
+        return notificationReactiveRepository.findAllByReceiverIdAndReadStatus(
+                memberId, false)
+                .onBackpressureBuffer()
+                .doOnError( error -> log.error("Id {} 에 대한 알림을 처리하는 동안 오류가 발생했습니다",memberId));
     }
 
 }
-
