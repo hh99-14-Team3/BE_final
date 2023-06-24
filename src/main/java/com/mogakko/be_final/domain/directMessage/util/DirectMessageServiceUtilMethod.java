c9b11bbfb89cdfd5cabdabcc94c6314411a3a0e2
been1118 <dsin1118@kakao.com>
2023-06-24 23:29:54 +0900

    Refactor : DirectMessage UtilMethod 분리

---
+++ src/main/java/com/mogakko/be_final/domain/directMessage/util/DirectMessageServiceUtilMethod.java
@@ -0,0 +1,20 @@
+package com.mogakko.be_final.domain.directMessage.util;
+
+import com.mogakko.be_final.domain.directMessage.entity.DirectMessage;
+import com.mogakko.be_final.domain.directMessage.repository.DirectMessageRepository;
+import com.mogakko.be_final.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+
+import static com.mogakko.be_final.exception.ErrorCode.MESSAGE_NOT_FOUND;
+
+@RequiredArgsConstructor
+public class DirectMessageServiceUtilMethod {
+
+    private DirectMessageRepository directMessageRepository;
+
+    public DirectMessage findDirectMessageById(Long id) {
+        return directMessageRepository.findById(id).orElseThrow(
+                () -> new CustomException(MESSAGE_NOT_FOUND)
+        );
+    }
+}
