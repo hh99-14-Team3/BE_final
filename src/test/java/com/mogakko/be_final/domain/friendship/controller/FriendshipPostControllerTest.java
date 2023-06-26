64741bd5a50139044257cb04a359460ef035f1d4
mansooonuna <parkdasom95@gmail.com>
2023-06-26 20:17:57 +0900

    Test : FriendshipPostController 친구요청(닉네임) 테스트코드

---
+++ src/test/java/com/mogakko/be_final/domain/friendship/controller/FriendshipPostControllerTest.java
@@ -0,0 +1,74 @@
+package com.mogakko.be_final.domain.friendship.controller;
+
+import com.mogakko.be_final.domain.friendship.dto.request.FriendRequestDto;
+import com.mogakko.be_final.domain.friendship.service.FriendshipPostService;
+import com.mogakko.be_final.domain.members.entity.MemberStatusCode;
+import com.mogakko.be_final.domain.members.entity.Members;
+import com.mogakko.be_final.domain.members.entity.Role;
+import com.mogakko.be_final.domain.mogakkoRoom.controller.MogakkoGetController;
+import com.mogakko.be_final.domain.mogakkoRoom.dto.request.MogakkoRoomCreateRequestDto;
+import com.mogakko.be_final.domain.mogakkoRoom.entity.LanguageEnum;
+import com.mogakko.be_final.domain.mogakkoRoom.service.MogakkoGetService;
+import com.mogakko.be_final.userDetails.UserDetailsImpl;
+import com.mogakko.be_final.util.Message;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Friendship Controller - [POST] 테스트")
+class FriendshipPostControllerTest {
+
+    @Mock
+    private FriendshipPostService friendshipPostService;
+    @Mock
+    private FriendRequestDto friendRequestDto;
+    @InjectMocks
+    private FriendshipPostController friendshipPostController;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(friendshipPostController).build();
+    }
+
+    Members member = Members.builder()
+            .email("test@example.com")
+            .nickname("nickname")
+            .password("password1!")
+            .role(Role.USER)
+            .codingTem(36.5)
+            .mogakkoTotalTime(0L)
+            .memberStatusCode(MemberStatusCode.BASIC)
+            .profileImage("https://source.boringavatars.com/beam/120/$" + "nickname" + "?colors=00F0FF,172435,394254,EAEBED,F9F9FA")
+            .friendCode(123456)
+            .isTutorialCheck(false)
+            .build();
+
+    @DisplayName("[POST] 닉네임으로 친구 요청 테스트")
+    @Test
+    void friendRequest() throws Exception {
+        UserDetailsImpl userDetails = new UserDetailsImpl(member, member.getEmail());
+        Message message = new Message("친구 요청 완료", null);
+        when(friendshipPostService.friendRequest(any(FriendRequestDto.class), any(Members.class))).thenReturn(ResponseEntity.ok(message));
+
+        ResponseEntity<Message> response = friendshipPostController.friendRequest(friendRequestDto, userDetails);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(message, response.getBody());
+    }
+
+}
