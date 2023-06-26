22a9337e95d0b82286b8e0783a73cfb233a8e4c6
been1118 <dsin1118@kakao.com>
2023-06-26 21:39:20 +0900

    Refactor : MogakkoPutService 요일별 타이머 저장하는 switch문 메서드 분리

--- src/main/java/com/mogakko/be_final/domain/mogakkoRoom/service/MogakkoPutService.java
+++ src/main/java/com/mogakko/be_final/domain/mogakkoRoom/service/MogakkoPutService.java
@@ -6,9 +6,11 @@ import com.mogakko.be_final.domain.members.entity.Members;
 import com.mogakko.be_final.domain.members.repository.MemberWeekStatisticsRepository;
 import com.mogakko.be_final.domain.members.repository.MembersRepository;
 import com.mogakko.be_final.domain.mogakkoRoom.dto.request.MogakkoTimerRequestDto;
+import com.mogakko.be_final.domain.mogakkoRoom.util.MogakkoServiceUtilMethod;
 import com.mogakko.be_final.exception.CustomException;
 import com.mogakko.be_final.util.Message;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,13 +19,16 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.Time;
 import java.time.LocalDateTime;
 
+import static com.mogakko.be_final.exception.ErrorCode.INTERNAL_SERER_ERROR;
 import static com.mogakko.be_final.exception.ErrorCode.USER_NOT_FOUND;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MogakkoPutService {
     private final MembersRepository membersRepository;
     private final MemberWeekStatisticsRepository memberWeekStatisticsRepository;
+    private final MogakkoServiceUtilMethod mogakkoServiceUtilMethod;
 
     // 타이머
     @Transactional
@@ -32,23 +37,14 @@ public class MogakkoPutService {
         if (Long.parseLong(mogakkoTimerRequestDto.getMogakkoTimer().substring(0, 2)) > 23)
             mogakkoTimer = new Time(20, 0, 0);
         else mogakkoTimer = Time.valueOf(mogakkoTimerRequestDto.getMogakkoTimer());
-        long timeToSec = mogakkoTimer.toLocalTime().toSecondOfDay();
 
-        int dayOfWeek = LocalDateTime.now().getDayOfWeek().getValue();
+        long timeToSec = mogakkoTimer.toLocalTime().toSecondOfDay();
 
         MemberWeekStatistics memberWeekStatistic = memberWeekStatisticsRepository.findById(member.getEmail()).orElseThrow(
                 () -> new CustomException(USER_NOT_FOUND)
         );
 
-        switch (dayOfWeek) {
-            case 1 -> memberWeekStatistic.addMon(timeToSec);
-            case 2 -> memberWeekStatistic.addTue(timeToSec);
-            case 3 -> memberWeekStatistic.addWed(timeToSec);
-            case 4 -> memberWeekStatistic.addThu(timeToSec);
-            case 5 -> memberWeekStatistic.addFri(timeToSec);
-            case 6 -> memberWeekStatistic.addSat(timeToSec);
-            case 7 -> memberWeekStatistic.addSun(timeToSec);
-        }
+        addTimeToWeek(LocalDateTime.now().getDayOfWeek().getValue(), timeToSec, memberWeekStatistic);
 
         member.setTime(timeToSec);
         long totalTimer = member.getMogakkoTotalTime() + timeToSec;
@@ -71,5 +67,22 @@ public class MogakkoPutService {
 
         membersRepository.save(member);
         return new ResponseEntity<>(new Message("저장 성공", mogakkoTimer), HttpStatus.OK);
+    }
+
+    @Transactional
+    void addTimeToWeek(int dayOfWeek, long timeToSec, MemberWeekStatistics memberWeekStatistic) {
+        switch (dayOfWeek) {
+            case 1 -> memberWeekStatistic.addMon(timeToSec);
+            case 2 -> memberWeekStatistic.addTue(timeToSec);
+            case 3 -> memberWeekStatistic.addWed(timeToSec);
+            case 4 -> memberWeekStatistic.addThu(timeToSec);
+            case 5 -> memberWeekStatistic.addFri(timeToSec);
+            case 6 -> memberWeekStatistic.addSat(timeToSec);
+            case 7 -> memberWeekStatistic.addSun(timeToSec);
+            default -> {
+                log.error("===========addTimeToWeek Method Invalid Input Error");
+                throw new IllegalArgumentException(String.valueOf(INTERNAL_SERER_ERROR));
+            }
+        }
     }
 }
