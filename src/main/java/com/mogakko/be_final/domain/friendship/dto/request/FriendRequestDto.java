1a53268750eb86489ec8a36c3045802c41269c94
mansooonuna <parkdasom95@gmail.com>
2023-06-27 16:51:40 +0900

    Refactor : FriendRequest 관련 Dto @Builder 추가

--- src/main/java/com/mogakko/be_final/domain/friendship/dto/request/FriendRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/dto/request/FriendRequestDto.java
@@ -1,9 +1,13 @@
 package com.mogakko.be_final.domain.friendship.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class FriendRequestDto {
     private String requestReceiverNickname;
