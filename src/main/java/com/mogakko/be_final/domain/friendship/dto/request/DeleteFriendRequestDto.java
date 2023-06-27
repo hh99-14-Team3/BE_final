c2923d5b643c3c2bbc06235404fe6c4103330e0f
mansooonuna <parkdasom95@gmail.com>
2023-06-27 17:26:57 +0900

    Refactor : DeleteFriendRequestDto에 @Builder 추가

--- src/main/java/com/mogakko/be_final/domain/friendship/dto/request/DeleteFriendRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/friendship/dto/request/DeleteFriendRequestDto.java
@@ -1,12 +1,16 @@
 package com.mogakko.be_final.domain.friendship.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class DeleteFriendRequestDto {
     private List<String> receiverNickname;
 }
