6adbda2c185468c88e099ac04e94a31bcf3fbcea
been1118 <dsin1118@kakao.com>
2023-06-28 00:13:46 +0900

    Test : BadWordFilteringTest checkBadWord 욕설 필터링 테스트 케이스 작성

---
+++ src/test/java/com/mogakko/be_final/util/BadWordFilteringTest.java
@@ -0,0 +1,38 @@
+package com.mogakko.be_final.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BadWordFilteringTest {
+
+    @InjectMocks
+    BadWordFiltering badWordFiltering;
+
+    @Nested
+    @DisplayName("checkBadWord Method 테스트")
+    class CheckBadWord {
+        @DisplayName("욕설 필터링 케이스")
+        @Test
+        void checkBadWord_filteringBadWord() {
+            // given
+            String str = "시3232발.";
+
+            // when
+            String filteringStr = badWordFiltering.checkBadWord(str);
+
+            // then
+            assertEquals("******.", filteringStr);
+        }
+    }
+
+    @Test
+    void checkBadWordUUID() {
+    }
+}
