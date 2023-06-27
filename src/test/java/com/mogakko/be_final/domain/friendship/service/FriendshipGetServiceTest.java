68f5cce7660390f2592440c1fcec457fb3917597
mansooonuna <parkdasom95@gmail.com>
2023-06-27 16:17:05 +0900

    Test : FriendshipGetService 친구목록조회2 테스트코드

--- src/test/java/com/mogakko/be_final/domain/friendship/service/FriendshipGetServiceTest.java
+++ src/test/java/com/mogakko/be_final/domain/friendship/service/FriendshipGetServiceTest.java
@@ -1,5 +1,6 @@
 package com.mogakko.be_final.domain.friendship.service;
 
+import com.mogakko.be_final.domain.friendship.dto.response.FriendResponseDto;
 import com.mogakko.be_final.domain.friendship.entity.Friendship;
 import com.mogakko.be_final.domain.friendship.entity.FriendshipStatus;
 import com.mogakko.be_final.domain.friendship.repository.FriendshipRepository;
@@ -13,12 +14,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -41,7 +40,7 @@ class FriendshipGetServiceTest {
             .email("test@example.com")
             .nickname("nickname")
             .password("password1!")
-            .role(Role.ADMIN)
+            .role(Role.USER)
             .codingTem(36.5)
             .mogakkoTotalTime(0L)
             .memberStatusCode(MemberStatusCode.BASIC)
@@ -50,16 +49,56 @@ class FriendshipGetServiceTest {
             .isTutorialCheck(false)
             .build();
 
-    @DisplayName("[GET] 친구 목록 조회 성공 테스트 - 조회된 친구 없음")
+    Members receiver = Members.builder()
+            .id(2L)
+            .email("test1@example.com")
+            .nickname("nickname1")
+            .password("password2!")
+            .role(Role.USER)
+            .codingTem(36.5)
+            .mogakkoTotalTime(0L)
+            .memberStatusCode(MemberStatusCode.BASIC)
+            .profileImage("https://source.boringavatars.com/beam/120/$" + "nickname" + "?colors=00F0FF,172435,394254,EAEBED,F9F9FA")
+            .friendCode(123456)
+            .isTutorialCheck(false)
+            .build();
+
+    @DisplayName("[GET] 친구 목록 조회 성공 테스트")
     @Test
     void getMyFriend() {
+        // Given
         List<Friendship> friendshipList = new ArrayList<>();
+        Friendship friendship1 = Friendship.builder().id(1L).sender(member).receiver(receiver).status(FriendshipStatus.ACCEPT).build();
+        Friendship friendship2 = Friendship.builder().id(2L).sender(receiver).receiver(member).status(FriendshipStatus.ACCEPT).build();
+        friendshipList.add(friendship1);
+        friendshipList.add(friendship2);
         when(friendshipRepository.findAllByReceiverAndStatusOrSenderAndStatus(member, FriendshipStatus.ACCEPT, member, FriendshipStatus.ACCEPT)).thenReturn(friendshipList);
+
+        // When
+        List<FriendResponseDto> friendsList = new ArrayList<>();
+        FriendResponseDto responseDto = new FriendResponseDto(member, false);
+        friendsList.add(responseDto);
+        when(membersRepository.findById(receiver.getId())).thenReturn(Optional.of(receiver));
 
         ResponseEntity<Message> response = friendshipGetService.getMyFriend(member);
 
+        // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("친구 목록 조회 성공", response.getBody().getMessage());
+    }
+
+    @DisplayName("[GET] 친구 목록 조회 성공 테스트 - 조회된 친구 없음")
+    @Test
+    void getMyFriend_NotFoundFriends() {
+        // Given
+        List<Friendship> friendshipList = new ArrayList<>();
+        when(friendshipRepository.findAllByReceiverAndStatusOrSenderAndStatus(member, FriendshipStatus.ACCEPT, member, FriendshipStatus.ACCEPT)).thenReturn(friendshipList);
+
+        // When
+        ResponseEntity<Message> response = friendshipGetService.getMyFriend(member);
+
+        // Then
         assertEquals(response.getStatusCode(), HttpStatus.OK);
         assertEquals(response.getBody().getMessage(), "조회된 친구가 없습니다.");
     }
-
 }
