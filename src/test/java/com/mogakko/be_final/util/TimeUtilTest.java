9bf56ec94f02cfe97d81942930216eea238a308a
been1118 <dsin1118@kakao.com>
2023-06-27 23:59:42 +0900

    Test : TimeUtilTest 인스턴스 생성 테스트 케이스 작성

---
+++ src/test/java/com/mogakko/be_final/util/TimeUtilTest.java
@@ -0,0 +1,24 @@
+package com.mogakko.be_final.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TimeUtilTest {
+
+    @Nested
+    @DisplayName("TImeUtil class 테스트")
+    class Time {
+        @DisplayName("TimeUtil 테스트")
+        @Test
+        void timeUtil_test() {
+             TimeUtil timeUtil = new TimeUtil();
+        }
+    }
+}
