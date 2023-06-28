126c381ea25a3185db65f510bc95de0ada6a94ff
been1118 <dsin1118@kakao.com>
2023-06-28 13:07:19 +0900

    Test : NotificationSendServiceTest sendFriendRequestNotification Method 테스트 케이스 작성

---
+++ src/test/java/com/mogakko/be_final/domain/sse/service/NotificationSendServiceTest.java
@@ -0,0 +1,88 @@
+package com.mogakko.be_final.domain.sse.service;
+
+import com.mogakko.be_final.domain.members.entity.MemberStatusCode;
+import com.mogakko.be_final.domain.members.entity.Members;
+import com.mogakko.be_final.domain.members.entity.Role;
+import com.mogakko.be_final.domain.members.entity.SocialType;
+import com.mogakko.be_final.domain.sse.entity.NotificationType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith({MockitoExtension.class})
+class NotificationSendServiceTest {
+
+    @Value("${SERVER_URL}")
+    private String SERVER_URL;
+
+    @Mock
+    NotificationService notificationService;
+    @InjectMocks
+    NotificationSendService notificationSendService;
+
+    Members sender = Members.builder()
+            .id(1L)
+            .email("test@example.com")
+            .nickname("nickname")
+            .memberStatusCode(MemberStatusCode.BASIC)
+            .mogakkoTotalTime(0L)
+            .githubId("github")
+            .profileImage("image")
+            .role(Role.USER)
+            .socialUid("id")
+            .socialType(SocialType.GOOGLE)
+            .password("1q2w3e4r")
+            .codingTem(36.5)
+            .build();
+
+    Members receiver = Members.builder()
+            .id(2L)
+            .email("test@test.com")
+            .nickname("nickname2")
+            .memberStatusCode(MemberStatusCode.BASIC)
+            .mogakkoTotalTime(0L)
+            .githubId("github")
+            .profileImage("image")
+            .role(Role.USER)
+            .socialUid("id")
+            .socialType(SocialType.GOOGLE)
+            .password("1q2w3e4r")
+            .codingTem(36.5)
+            .build();
+
+    @Nested
+    @DisplayName("sendFriendRequestNotification Method 테스트")
+    class SendFriendRequestNotification {
+        @DisplayName("sendFriendRequestNotification 성공 테스트")
+        @Test
+        void sendFriendRequestNotification() {
+            // when
+            notificationSendService.sendFriendRequestNotification(sender, receiver);
+
+            // then
+            verify(notificationService)
+                    .send(sender, receiver, NotificationType.FRIEND_REQUEST, sender.getNickname() + "님이 친구요청을 보냈습니다.", SERVER_URL + "/friendship/requests/pending");
+        }
+    }
+
+    @Test
+    void sendAcceptNotification() {
+    }
+
+    @Test
+    void sendRefuseNotification() {
+    }
+
+    @Test
+    void sendMessageReceivedNotification() {
+    }
+}
