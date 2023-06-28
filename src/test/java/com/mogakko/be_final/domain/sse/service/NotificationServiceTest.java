4860b7a71a49c1b82e0b6bbf6e6951db7bd21f24
been1118 <dsin1118@kakao.com>
2023-06-28 11:10:43 +0900

    Test : NotificationServiceTest subscribe Method sseEmitter onCompletion 테스트 케이스 작성

--- src/test/java/com/mogakko/be_final/domain/sse/service/NotificationServiceTest.java
+++ src/test/java/com/mogakko/be_final/domain/sse/service/NotificationServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -155,6 +156,40 @@ class NotificationServiceTest {
             // when & then
             CustomException customException =  assertThrows(CustomException.class, ()-> notificationService.subscribe(memberId, lastEventId));
             assertEquals(USER_NOT_FOUND, customException.getErrorCode());
+        }
+
+        @DisplayName("subscribe Method sseEmitter Completion 테스트")
+        @Test
+        void subscribe_emitterOnCompletion() {
+            // given
+            Long memberId1 = 1L;
+            SseEmitter sseEmitter = mock(SseEmitter.class);
+
+            List<Notification> notificationList = new ArrayList<>();
+            notificationList.add(notification);
+
+            List<Members> membersList = new ArrayList<>();
+            membersList.add(member2);
+
+            when(emitterRepository.save(any(), any())).thenReturn(sseEmitter);
+            when(membersRepository.findById(memberId1)).thenReturn(Optional.of(member1));
+            when(notificationRepository.findAllByReceiverIdAndReadStatusAndCreatedAtLessThan(any(), eq(false), any()))
+                    .thenReturn(notificationList);
+            for (int i = 0; i < notificationList.size(); i++) {
+                when(membersRepository.findByNickname(notificationList.get(i).getSenderNickname()))
+                        .thenReturn(Optional.of(membersList.get(i)));
+            }
+            ArgumentCaptor<Runnable> completionCaptor = ArgumentCaptor.forClass(Runnable.class);
+            doNothing().when(sseEmitter).onCompletion(completionCaptor.capture());
+            doNothing().when(emitterRepository).deleteById(anyString());
+
+            // when
+            notificationService.subscribe(1L, "lastEventId");
+
+            // then
+            Runnable completionTask = completionCaptor.getValue();
+            completionTask.run();
+            verify(emitterRepository, times(1)).deleteById(anyString());
         }
     }
 
