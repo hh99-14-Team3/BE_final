497199a204b7d81e4d026e193083b8939dbc20fe
been1118 <dsin1118@kakao.com>
2023-06-30 00:25:02 +0900

    Test : CustomOAuth2UserServiceTest getMembers 기존 유저 테스트 케이스 작성

--- src/test/java/com/mogakko/be_final/security/oauth2/service/CustomOAuth2UserServiceTest.java
+++ src/test/java/com/mogakko/be_final/security/oauth2/service/CustomOAuth2UserServiceTest.java
@@ -1,8 +1,13 @@
 package com.mogakko.be_final.security.oauth2.service;
 
+import com.mogakko.be_final.domain.members.entity.MemberStatusCode;
+import com.mogakko.be_final.domain.members.entity.Members;
+import com.mogakko.be_final.domain.members.entity.Role;
 import com.mogakko.be_final.domain.members.entity.SocialType;
 import com.mogakko.be_final.domain.members.repository.MembersRepository;
 import com.mogakko.be_final.exception.CustomException;
+import com.mogakko.be_final.security.oauth2.userinfo.OAuth2UserInfo;
+import com.mogakko.be_final.security.oauth2.util.OAuthAttributes;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,17 +16,40 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static com.mogakko.be_final.exception.ErrorCode.NOT_SUPPORTED_SOCIALTYPE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
 class CustomOAuth2UserServiceTest {
 
     @Mock
+    OAuth2UserInfo oauth2UserInfo;
+    @Mock
     MembersRepository membersRepository;
     @InjectMocks
     CustomOAuth2UserService customOAuth2UserService;
+
+    Members member = Members.builder()
+            .id(1L)
+            .email("test@example.com")
+            .nickname("nickname")
+            .memberStatusCode(MemberStatusCode.BASIC)
+            .mogakkoTotalTime(0L)
+            .githubId("github")
+            .profileImage("image")
+            .role(Role.USER)
+            .socialUid("id")
+            .socialType(SocialType.GOOGLE)
+            .password("1q2w3e4r")
+            .codingTem(36.5)
+            .build();
 
     @Nested
     @DisplayName("getSocialType Method 테스트")
@@ -72,11 +100,32 @@ class CustomOAuth2UserServiceTest {
             String registrationId = "naver";
 
             // when
-            CustomException customException = assertThrows(CustomException.class, ()-> customOAuth2UserService.getSocialType(registrationId));
+            CustomException customException = assertThrows(CustomException.class, () -> customOAuth2UserService.getSocialType(registrationId));
 
             // then
             assertEquals(NOT_SUPPORTED_SOCIALTYPE, customException.getErrorCode());
         }
     }
 
+    @Nested
+    @DisplayName("getMembers Method 테스트")
+    class GetMembers {
+        @DisplayName("getMembers 기존 유저 테스트")
+        @Test
+        void getMembers() {
+            // given
+            OAuthAttributes oAuthAttributes = mock(OAuthAttributes.class);
+            SocialType socialType = SocialType.GOOGLE;
+
+            when(oAuthAttributes.getOauth2UserInfo()).thenReturn(oauth2UserInfo);
+            when(oauth2UserInfo.getId()).thenReturn("id");
+            when(membersRepository.findBySocialUidAndSocialType(any(), eq(socialType))).thenReturn(Optional.of(member));
+
+            // when
+            Members response = customOAuth2UserService.getMembers(oAuthAttributes, socialType);
+
+            // then
+            assertEquals(member, response);
+        }
+    }
 }
