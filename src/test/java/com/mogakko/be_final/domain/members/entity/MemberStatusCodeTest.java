dfb1135d9819ed0e8365e21d5495a5d3d78872b1
been1118 <dsin1118@kakao.com>
2023-06-29 22:28:50 +0900

    Test : MemberStatusCodeTest from Method 성공 테스트 케이스 작성

---
+++ src/test/java/com/mogakko/be_final/domain/members/entity/MemberStatusCodeTest.java
@@ -0,0 +1,31 @@
+package com.mogakko.be_final.domain.members.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith({MockitoExtension.class})
+class MemberStatusCodeTest {
+
+    @Nested
+    @DisplayName("from Method 테스트")
+    class From {
+        @DisplayName("from Method 성공 테스트")
+        @Test
+        void from_success() {
+            // given
+            String value = "200";
+
+            // when
+            MemberStatusCode memberStatusCode = MemberStatusCode.from(value);
+
+            // then
+            assertEquals(MemberStatusCode.NORMAL, memberStatusCode);
+        }
+
+    }
+}
