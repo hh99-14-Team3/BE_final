a01f87cb2f837d54630ce485c24b65d442edc3f8
mansooonuna <parkdasom95@gmail.com>
2023-06-29 23:27:13 +0900

    Chore : 불필요한 import 제거

--- src/test/java/com/mogakko/be_final/S3/S3UploaderTest.java
+++ src/test/java/com/mogakko/be_final/S3/S3UploaderTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
