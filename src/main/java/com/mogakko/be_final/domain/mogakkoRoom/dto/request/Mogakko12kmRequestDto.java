99b574c9f7d6ab73186a35c1422ddb1d493ebed5
been1118 <dsin1118@kakao.com>
2023-06-29 17:20:10 +0900

    Chore : 안쓰는 class, mapper 삭제 (MapStruct, MogakkoRoomResponseDto)

--- src/main/java/com/mogakko/be_final/domain/mogakkoRoom/dto/request/Mogakko12kmRequestDto.java
+++ src/main/java/com/mogakko/be_final/domain/mogakkoRoom/dto/request/Mogakko12kmRequestDto.java
@@ -3,10 +3,12 @@ package com.mogakko.be_final.domain.mogakkoRoom.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Mogakko12kmRequestDto {
     private double lon;
     private double lat;
